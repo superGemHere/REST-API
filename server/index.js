94ff7f93cf5925ac67eeb1fdf85ffd6755881bd7
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-04 23:20:38 +0200

    allow CORS requests

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
-const routes = require('./routes')
+const corsOptions = require('./corsPolicy');
+
+const routes = require('./routes');
 
 const app = express();
 
+//Here you are restricted to my CORS policy.  If you want a different one, change the variable or leave it with empty argument field for default behaviour.
+app.use(cors(corsOptions))
+
 app.get('/', (req, res) => {
+    // console.log(corsOptions)
     res.send('Hello REST API')
 });
 
