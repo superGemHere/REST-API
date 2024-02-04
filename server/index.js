17a56ec52ba9f1b2481563b2886d989f42161648
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-04 19:49:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello REST API')
+})
+
 
 
 app.listen(3030, () => console.log('Server is listening on port: 3030...'));
