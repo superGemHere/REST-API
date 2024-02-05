fca701b116beb38aff21551267dd8dc2c3214c49
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-05 22:03:40 +0200

    Create constants.js

---
+++ server/constants.js
@@ -0,0 +1,10 @@
+const SECRET_JWT = 'f05876bea883f0e7397da87a12c31c0d';
+const SALT_ROUNDS= 10;
+const CONNECTION__DB= "mongodb://localhost:27017/example";
+const CLIENT__PORT = 3030;
+
+exports.SECRET_JWT = SECRET_JWT;
+exports.SALT_ROUNDS = SALT_ROUNDS;
+exports.CONNECTION__DB = CONNECTION__DB;
+exports.CLIENT__PORT = CLIENT__PORT;
+
