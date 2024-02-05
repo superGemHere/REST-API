ae0cb07cef303c287bf714a34e329ac22ca650b7
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-02-05 22:03:44 +0200

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
+const userManager = require('../managers/userManager')
+
+router.post('/register', async (req, res) => {
+    const {username, email, password, repeatPassword} = req.body;
+    const token = await userManager.register(username, email, password, repeatPassword);
+
+    // res.cookie('auth', token);
+
+})
+router.post('/login', async (req, res) => {
+    const { email, password} = req.body;
+
+    const result = await userManager.login(email, password);
+
+    res.json(result)
+})
+router.get('/logout', (req, res) => {
 
 })
 
